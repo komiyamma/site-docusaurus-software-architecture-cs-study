e8b5f33d5a47b2dd3864d64a260cd19b67ab216a
komiyamma <komiyama0629@gmail.com>
2026-03-08 23:16:24 +0900

    fix(home): メタ記述をサイトタグラインに切り替え

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -11,7 +11,7 @@ export default function Home(): ReactNode {
   return (
     <Layout
       title={`${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      description={siteConfig.tagline}>
       <WebSiteJsonLd />
       <main>
         <HomepageFeatures CategorySvg={CategorySvg} />
